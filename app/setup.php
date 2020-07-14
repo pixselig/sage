4612b317bbe67e3013deadbf53bc9f7c36af80a2
pixselig <pixselig@users.noreply.github.com>
2020-07-14 19:18:35 +0200

    BugFix: Rest-API

--- app/setup.php
+++ app/setup.php
@@ -52,7 +52,7 @@ add_action( 'after_setup_theme',
         add_theme_support( 'soil-nav-walker' );
         add_theme_support( 'soil-nice-search' );
         add_theme_support( 'soil-relative-urls' );
-        add_theme_support( 'soil-disable-rest-api' );
+        //add_theme_support( 'soil-disable-rest-api' );
         add_theme_support( 'soil-disable-asset-versioning' );
         add_theme_support( 'soil-disable-trackbacks' );
         add_theme_support( 'soil-js-to-footer' );
